6175bc23fffdc4e9014ac614a3132b67c7327ec8
Kirill Koval <brcyrill2@gmail.com>
2024-01-03 03:47:05 +0200

    found solution

--- src/main/java/com/kerrrusha/codewars/BecomeImmortal.java
+++ src/main/java/com/kerrrusha/codewars/BecomeImmortal.java
@@ -1,23 +1,35 @@
 package com.kerrrusha.codewars;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BecomeImmortal {
     public static long elderAge(long n, long m, long k, long newp) {
-        List<List<Long>> magicRectangle = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            List<Long> row = new ArrayList<>();
-            for (int j = 0; j < m; j++) {
-                long xor = i ^ j;
-                long res = xor - k;
-                row.add(res < 0 ? 0 : res);
+        long result = 0;
+        while (n > 0) {
+            if (n < m) {
+                long temp = n;
+                n = m;
+                m = temp;
             }
-            magicRectangle.add(row);
+            long shift = 1;
+            for (; 2 * shift <= n; shift *= 2) {}
+            if (m <= shift) {
+                result = (result + m % newp * sumRow(shift, k, newp)) % newp;
+                k -= shift;
+            } else {
+                result = (result + shift % newp * sumRow(shift, k, newp)) % newp;
+                result = (result + (n + m - 2 * shift) % newp * sumRow(shift, k - shift, newp)) % newp;
+                m -= shift;
+            }
+            n -= shift;
         }
-        return magicRectangle.stream()
-                .flatMap(List::stream)
-                .mapToLong(Long::longValue)
-                .sum() % newp;
+        return result;
+    }
+
+    private static long sumRow(long shift, long k, long newp) {
+        long n = 0 < k ? 0 : -k;
+        shift = shift < k ? 0 : shift - k;
+        if ((shift - n) % 2 == 0) {
+            return (shift - n) / 2 % newp * ((shift + n - 1) % newp) % newp;
+        }
+        return (((shift - n - 1) / 2 % newp * ((shift + n) % newp) % newp) + n) % newp;
     }
 }
